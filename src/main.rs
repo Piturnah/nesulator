37914cb2a2b71c9d7523adedb9762ae7422f5d5f
Piturnah <peterhebden6@gmail.com>
2022-06-24 00:47:53 +0100

    Implement OPC 0x89 (ADC_IM)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{fmt, ops::Shl};
 
 const NOP: u8 = 0xea;
+const ADC_IM: u8 = 0x69;
 
 struct Mos6502 {
     addr: u16,
     mem: [u8; u16::MAX as usize + 1],
     current_instruction: Option<u8>,
+    ra: u8,
 }
 
 impl Mos6502 {
@@ -14,6 +16,7 @@ impl Mos6502 {
             mem,
             addr: 0,
             current_instruction: None,
+            ra: 0,
         };
         cpu.reset();
         cpu
@@ -24,6 +27,14 @@ impl Mos6502 {
         self.addr = (self.mem[0xfffd] as u16).shl(8) + self.mem[0xfffc] as u16;
     }
 
+    // Fetch u8 from 16b address (little endian)
+    #[allow(dead_code)]
+    fn fetch(&mut self) -> u8 {
+        self.addr += 1;
+        self.mem[(self.mem[self.addr as usize - 1] as usize).shl(8)
+            + self.mem[self.addr as usize] as usize]
+    }
+
     fn cycle(&mut self) {
         match self.current_instruction {
             Some(NOP) => {
@@ -31,9 +42,18 @@ impl Mos6502 {
                 self.current_instruction = None;
                 self.addr += 1;
             }
+            Some(ADC_IM) => {
+                self.ra += self.mem[self.addr as usize];
+                self.current_instruction = None;
+                self.addr += 1;
+            }
             Some(code) => todo!("Implement handling of {}", code),
             None => match self.mem[self.addr as usize] {
                 NOP => self.current_instruction = Some(NOP),
+                ADC_IM => {
+                    self.current_instruction = Some(ADC_IM);
+                    self.addr += 1;
+                }
                 code => panic!("Instruction {} not handled", code),
             },
         }
@@ -60,5 +80,44 @@ fn main() {
     loop {
         println!("{:?}", cpu);
         cpu.cycle()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn adc_im() {
+        let mut rom = [NOP; u16::MAX as usize + 1];
+        rom[0] = ADC_IM;
+        rom[1] = 0x01;
+        rom[0xfffc] = 0;
+        rom[0xfffd] = 0;
+        let mut cpu = Mos6502::new(rom);
+
+        for _ in 0..2 {
+            cpu.cycle()
+        }
+
+        assert_eq!(cpu.ra, 0x01);
+    }
+
+    #[test]
+    fn adc_im_twice() {
+        let mut rom = [NOP; u16::MAX as usize + 1];
+        rom[0] = ADC_IM;
+        rom[1] = 0x01;
+        rom[2] = ADC_IM;
+        rom[3] = 0x01;
+        rom[0xfffc] = 0;
+        rom[0xfffd] = 0;
+        let mut cpu = Mos6502::new(rom);
+
+        for _ in 0..4 {
+            cpu.cycle()
+        }
+
+        assert_eq!(cpu.ra, 0x02);
     }
 }
